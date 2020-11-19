c88128d101e89c5c2a2ae5dfa06e1550a48cc443
Stefan van der Walt <stefanv@berkeley.edu>
2020-11-19 00:09:09 -0800

    Fix SSR bug; not sure why it gets undefined Job

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -4,37 +4,41 @@ import { Link } from "gatsby";
 import Badge from "./Badge";
 
 const Job = ({ job }) => {
+  if (job === undefined) {
+    console.log("We don't expect an empty job posting; aborting");
+    return <div>Empty job posting</div>;
+  }
   return (
-  <div className="job">
-    <div className="badges">
-      { job.badges &&
-        job.badges.map((badge) =>
-          <Badge key={badge} type={badge}/>
-        )
-      }
-      { (job.percentOSS === 100) &&
-        <Badge type="Pure OSS"/>
-      }
-    </div>
-    <Link to={`/job/${job.id}`}>
-      <div className="title">{ job.entity } / { job.title }</div>
-    </Link>
-    <div className="meta">
-      <div className="percentTime">This job is { job.percentTime }% time.</div>
-      <div className="percentOSS">
-        { job.percentOSS }% of that time is on open source
+    <div className="job">
+      <div className="badges">
+        { job.badges &&
+          job.badges.map((badge) =>
+            <Badge key={badge} type={badge}/>
+          )
+        }
+        { (job.percentOSS === 100) &&
+          <Badge type="Pure OSS"/>
+        }
       </div>
-      { job.deadline &&
-        <div className="deadline">
-          Application deadline: { job.deadline }
+      <Link to={`/job/${job.id}`}>
+        <div className="title">{ job.entity } / { job.title }</div>
+      </Link>
+      <div className="meta">
+        <div className="percentTime">This job is { job.percentTime }% time.</div>
+        <div className="percentOSS">
+          { job.percentOSS }% of that time is on open source
         </div>
-      }
+        { job.deadline &&
+          <div className="deadline">
+            Application deadline: { job.deadline }
+          </div>
+        }
+      </div>
+      <div
+        className="description"
+        dangerouslySetInnerHTML={{__html: job.description}}
+      />
     </div>
-    <div
-      className="description"
-      dangerouslySetInnerHTML={{__html: job.description}}
-    />
-  </div>
-  )};
-
+  );
+};
 export default Job;
