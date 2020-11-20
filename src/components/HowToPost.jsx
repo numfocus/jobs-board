e7ad91cb1c4fc9baca9904cd549112b6ae4f6afd
Stefan van der Walt <stefanv@berkeley.edu>
2020-11-19 16:56:00 -0800

    Update instructions for how to post

--- src/components/HowToPost.jsx
+++ src/components/HowToPost.jsx
@@ -2,10 +2,18 @@ import React from "react";
 
 const HowToPost = () => (
   <div className="howToPost">
-    To post a job to this board, please&nbsp;
-    <a href="https://github.com/numfocus/jobs-board">
-      file a post request
-    </a>.
+    <p>
+      To post a job to this board, please&nbsp;
+      <a href="https://github.com/numfocus/jobs-board">
+        file a pull request
+      </a>.
+    </p>
+    <p>
+      An example post template can be found&nbsp;
+      <a href="https://github.com/numfocus/jobs-board/blob/master/jobs/template.yaml">
+        here
+      </a>.
+    </p>
   </div>
 );
 
